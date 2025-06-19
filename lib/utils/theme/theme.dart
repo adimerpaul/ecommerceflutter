88bce7125767f31cedb2dc198f65ead35edd6357
adimerpaul <adimer101@gmail.com>
2025-06-19 06:58:22 -0400

    Ligt y dark

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,47 @@
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/theme/widgets_theme/appbar_theme.dart';
+import 'package:ecommerce/utils/theme/widgets_theme/bottom_sheet_theme.dart';
+import 'package:ecommerce/utils/theme/widgets_theme/checkbox_theme.dart';
+import 'package:ecommerce/utils/theme/widgets_theme/chip_theme.dart';
+import 'package:ecommerce/utils/theme/widgets_theme/elevated_button_theme.dart';
+import 'package:ecommerce/utils/theme/widgets_theme/outlined_button_theme.dart';
+import 'package:ecommerce/utils/theme/widgets_theme/text_Theme.dart';
+import 'package:ecommerce/utils/theme/widgets_theme/text_field_theme.dart';
+import 'package:flutter/material.dart';
+
+class UAppTheme {
+  UAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: '',
+    brightness: Brightness.light,
+    primaryColor:  UColors.primary,
+    disabledColor: UColors.grey,
+    textTheme: UTextTheme.lightTextTheme,
+    chipTheme: UChipTheme.lightChipTheme,
+    scaffoldBackgroundColor: UColors.white,
+    appBarTheme: UAppBarTheme.lightAppBarTheme,
+    checkboxTheme: UCheckboxTheme.lightCheckboxTheme,
+    bottomSheetTheme: UBottomSheetTheme.lightBottomSheetTheme,
+    elevatedButtonTheme: UElevatedButtonTheme.lightElevatedButtonTheme,
+    outlinedButtonTheme: UOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: UTextFormFieldTheme.lightInputDecorationTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: '',
+    brightness: Brightness.dark,
+    primaryColor: UColors.primary,
+    disabledColor: UColors.grey,
+    textTheme: UTextTheme.darkTextTheme,
+    chipTheme: UChipTheme.darkChipTheme,
+    scaffoldBackgroundColor: UColors.black,
+    appBarTheme: UAppBarTheme.darkAppBarTheme,
+    checkboxTheme: UCheckboxTheme.darkCheckboxTheme,
+    bottomSheetTheme: UBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme: UElevatedButtonTheme.darkElevatedButtonTheme,
+    outlinedButtonTheme: UOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: UTextFormFieldTheme.darkInputDecorationTheme,
+  );
+}
