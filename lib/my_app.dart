b0a4af0150eac4ae15589426985869f1f4d9a1fa
adimerpaul <adimer101@gmail.com>
2025-06-21 04:11:05 -0400

    creado boarding

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/features/authentications/screens/onboarding.dart';
 import 'package:ecommerce/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -7,17 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
       theme: UAppTheme.lightTheme,
       darkTheme: UAppTheme.darkTheme,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello, World!',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-        ),
-      ),
+      home: OnboardingScreen()
     );
   }
 }
